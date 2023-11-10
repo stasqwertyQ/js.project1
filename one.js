9311f02af46451330c03a24fb7fb2c062768592a
stasqwertyQ <125751548+stasqwertyQ@users.noreply.github.com>
2023-11-10 19:12:41 +0200

    js

---
+++ one.js
@@ -0,0 +1,20 @@
+const name = "Stas";
+const age = 15;
+var  isStudent = "true";
+var isnotStudent = "folse"
+const myString = "Завжди шукай позитив"
+const myNumber = 7;
+const myNumbers = myNumber + 10;
+const myNull = null;
+let prompttt = prompt("введення імені користувача");
+let session = confirm("Чи ви знахотитеся зараз за пк?");
+alert(session);
+alert ("Дія небезпечна просимо користувача підтвердити дію");
+let config = confirm("підтвердіть дію");
+// console//
+console.log(age)
+console.log(name)
+console.log(isStudent);
+console.log(myString);
+console.log(myNumbers);
+console.log(myNull);
